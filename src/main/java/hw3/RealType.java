55c262786f97eb28ef6d6e7e6a2bc91fa158f0cf
underey <andrey.sverkunov@gmail.com>
2015-02-16 00:17:44 +0200

    Homework3

---
+++ src/main/java/hw3/RealType.java
@@ -0,0 +1,19 @@
+package hw3;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RealType {
+
+	private Pattern pattern;
+	private Matcher matcher;
+	
+	private String regex = "[+-]?(?:\\d+\\.?\\d*|\\d*\\.\\d+)";
+	
+	public boolean verification(double number){
+		pattern = Pattern.compile(regex);
+		matcher = pattern.matcher(Double.toString(number));
+		return matcher.matches();
+	}
+		
+}
