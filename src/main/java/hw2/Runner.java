8ca557443c45e2a2ce87829c4b44086ae150ac5f
isverkunova <iren.sverkunova@gmail.com>
2015-02-16 09:40:00 +0200

    2-nd hw

---
+++ src/main/java/hw2/Runner.java
@@ -0,0 +1,41 @@
+package hw2;
+
+import java.util.Arrays;
+
+public class Runner {
+
+	static int num = 99;
+
+	public static void main(String[] args) {
+
+		int[] mass = new int[20];
+
+		System.out.println("Unsorted array: ");
+		for (int a = 0; a < mass.length; a++) {
+			mass[a] = (int) (Math.random() * 100);
+			System.out.print(mass[a] + " ");
+		}
+
+		System.out.println();
+
+		Arrays.sort(mass);
+		System.out.println("Sorted array: ");
+		for (int a = 0; a < mass.length; a++) {
+			System.out.print(mass[a] + " ");
+		}
+
+		System.out.println();
+
+		BinarySearch myClass = new BinarySearch(mass);
+		int result = myClass.finder(num);
+
+		if (result == num) {
+			System.out.println("Number " + num + " presents in the array.");
+		} else {
+			System.out.println("Number " + num + " is absent.");
+			System.out.println("The next number after searched is: " + result
+					+ ".");
+		}
+	}
+
+}
